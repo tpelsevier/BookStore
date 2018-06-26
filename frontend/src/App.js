2f1b800aa10209c41297474f37bd6b80a2109f41
pt657407064 <taopan1211@gmail.com>
2018-06-26 13:13:58 -0400

    add front end to master

---
+++ frontend/src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Header from './WebComponents/Header/Header';
+import AboutUs from './WebComponents/AboutUs/AboutUs';
+import Login from './WebComponents/Login/Login';
+import SignUp from './WebComponents/Signup/Signup';
+import BookDetails from './WebComponents/BookDetails/BookDetails';
+import Home from './WebComponents/Home/Home';
+import UserProfile from './WebComponents/UserProfile/UserProfile';
+
+class App extends Component {
+
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <header>
+            <Header />
+          </header>
+          <main>
+            <Switch>
+              <Route exact path='/home' component={Home} />
+              <Route exact path='/about_us' component={AboutUs} />
+              <Route exact path='/bookdetails' component={BookDetails}/>
+              <Route exact path='/login' component={Login} />
+              <Route exact path='/signup' component={SignUp}/>
+              <Route exact path='/BookDetails' component={BookDetails}/>
+              <Route exact path='/UserProfile' component={UserProfile}/>
+            </Switch>
+          </main>
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
---
+++ frontend/src/WebComponents/Signup/Signup.js
@@ -0,0 +1,113 @@
+import React, { Component } from "react";
+import { Button, FormGroup, FormControl, ControlLabel, ListGroup, ListGroupItem } from "react-bootstrap";
+import { signup } from '../../utils/APICalls';
+import "./SignUp.css";
+
+export default class SignUp extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            firstname: "",
+            lastname: "",
+            email: "",
+            password: ""
+        };
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    validateForm() {
+        return this.state.email.length > 0 && this.state.password.length > 0;
+    }
+
+    handleChange = event => {
+        this.setState({
+            [event.target.id]: event.target.value
+        });
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+        console.log(this.state.firstname);
+        console.log(this.state.lastname);
+        console.log(this.state.email);
+        console.log(this.state.password);
+
+        const signupRequest = {
+            firstname: this.state.firstname.value,
+            lastname: this.state.lastname.value,
+            email: this.state.email.value,
+            password: this.state.password.value
+        };
+
+        // signup(signupRequest)
+        //     .then(response => {
+        //         notification.success({
+        //             message: 'Polling App',
+        //             description: "Thank you! You're successfully registered. Please Login to continue!",
+        //         });
+        //         this.props.history.push("/login");
+        //     }).catch(error => {
+        //         notification.error({
+        //             message: 'Polling App',
+        //             description: error.message || 'Sorry! Something went wrong. Please try again!'
+        //         });
+        //     });
+    }
+
+    render() {
+        return (
+            <div className="Login">
+                <form onSubmit={this.handleSubmit}>
+                    <ListGroup>
+                        <ListGroupItem>
+                            <FormGroup controlId="firstname" bsSize="large">
+                                <ControlLabel>First Name</ControlLabel>
+                                <FormControl
+                                    autoFocus
+                                    type="firstname"
+                                    value={this.state.firstname}
+                                    onChange={this.handleChange}
+                                />
+                            </FormGroup>
+                            <FormGroup controlId="lastname" bsSize="large">
+                                <ControlLabel>Last Name</ControlLabel>
+                                <FormControl
+                                    autoFocus
+                                    type="lastname"
+                                    value={this.state.lastname}
+                                    onChange={this.handleChange}
+                                />
+                            </FormGroup>
+                            <FormGroup controlId="email" bsSize="large">
+                                <ControlLabel>Email</ControlLabel>
+                                <FormControl
+                                    value={this.state.email}
+                                    onChange={this.handleChange}
+                                    type="email"
+                                />
+                            </FormGroup>
+                            <FormGroup controlId="password" bsSize="large">
+                                <ControlLabel>Password</ControlLabel>
+                                <FormControl
+                                    value={this.state.password}
+                                    onChange={this.handleChange}
+                                    type="password"
+                                />
+                            </FormGroup>
+                            <Button
+                                block
+                                bsSize="large"
+                                // disabled={!this.validateForm()}
+                                type="submit"
+                            >
+                                Sign Up
+                        </Button>
+                        </ListGroupItem>
+                    </ListGroup>
+                </form>
+            </div>
+        );
+    }
+}
