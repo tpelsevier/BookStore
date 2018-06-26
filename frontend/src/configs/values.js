ee66a68d343c12f12e925554970f83bcf1c7dffc
pt657407064 <taopan1211@gmail.com>
2018-06-26 13:01:51 -0400

    Create REST api connector

---
+++ frontend/src/configs/values.js
@@ -0,0 +1,15 @@
+export const API_BASE_URL = 'http://localhost:5000/api'
+
+export const ACCESS_TOKEN = 'accessToken';
+
+
+export const FIRST_NAME_MIN_LENGTH = 2;
+export const LAST_NAME_MAX_LENGTH = 40;
+
+export const USERNAME_MIN_LENGTH = 2;
+export const USERNAME_MAX_LENGTH = 15;
+
+export const EMAIL_MAX_LENGTH = 40;
+
+export const PASSWORD_MIN_LENGTH = 6;
+export const PASSWORD_MAX_LENGTH = 20;
