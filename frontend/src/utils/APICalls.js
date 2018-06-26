6b97890d5b7cf865824de1eef6a352fba9a832ad
pt657407064 <taopan1211@gmail.com>
2018-06-26 16:08:04 -0400

    RENAME THE FILE NAME

--- frontend/src/utils/APICalls.js
+++ frontend/src/utils/APICalls.js
@@ -5,7 +5,7 @@ const request = (options) => {
     const headers = new Headers({
         'Content-Type': 'application/json',
     })
-    
+
     if(localStorage.getItem(ACCESS_TOKEN)) {
         headers.append('Authorization', 'Bearer ' + localStorage.getItem(ACCESS_TOKEN))
     }
@@ -14,7 +14,7 @@ const request = (options) => {
     options = Object.assign({}, defaults, options);
 
     return fetch(options.url, options)
-    .then(response => 
+    .then(response =>
         response.json().then(json => {
             if(!response.ok) {
                 return Promise.reject(json);
